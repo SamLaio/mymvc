b7393e10f4470af7d4b37728d5301cb9a2644639
SamLaio <f85122@gmail.com>
2014-09-19 16:50:24 +0800

    0.3.7

--- control/install.php
+++ control/install.php
@@ -5,7 +5,38 @@ class install {
 		
 	}
 	function St1($arr){
-		print_r($arr);
+			/*
+				$DbType = 'mysql';
+				$DbHost = '192.168.247.33';
+				$DbUser = 'root';
+				$DbName = 'mymvc';
+				$DbPw = 'phitech';
+			 */
+		if(isset($arr['post']['DbType']) and $arr['post']['DbType'] != '')
+			$str = '$'."DbType = '".$arr['post']['DbType']."';\n";
+		if(isset($arr['post']['DbHost']) and $arr['post']['DbHost'] != '')
+			$str .= '$'."DbHost = '".$arr['post']['DbHost']."';\n";
+		if(isset($arr['post']['DbUser']) and $arr['post']['DbUser'] != '')
+			$str .= '$'."DbUser = '".$arr['post']['DbUser']."';\n";
+		if(isset($arr['post']['DbName']) and $arr['post']['DbName'] != ''){
+			if($arr['post']['DbType'] != 'sqlite')
+				$str .= '$'."DbName = '".$arr['post']['DbName']."';\n";
+			else
+				$str .= '$'."DbName = 'model/".$arr['post']['DbName']."';\n";
+		}
+		
+			
+		$str = "<?php\n".$str."?>";
+		$fp = fopen('lib/Config.php','w+');
+		//$file = fopen("test.xml","a+"); //開啟檔案
+		fwrite($fp,$str);
+		fclose($fp);
+		$this->St2();
+	}
+	private function St2(){
+		include 'model/install.php';
+		$installObj = new ModeInstall;
+		$installObj->St1();
 	}
 }
 
